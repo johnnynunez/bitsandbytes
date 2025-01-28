dd0cb9001890f705cd3f27e0d4dc6186b262549c
Johnny <johnnynuca14@gmail.com>
2025-01-29 00:41:51 +0100

    fix

--- .github/scripts/build-cpu.sh
+++ .github/scripts/build-cpu.sh
@@ -6,16 +6,14 @@ set -xeuo pipefail
 
 pip install cmake==3.28.3
 
-if [ "${build_os:0:6}" == ubuntu ] && [ "${build_arch}" == aarch64 ]; then
-	# Allow cross-compile on aarch64
-	sudo apt-get update
-	sudo apt-get install -y gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu g++-aarch64-linux-gnu
-	cmake -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc -DCMAKE_CXX_COMPILER=aarch64-linux-gnu-g++ -DCOMPUTE_BACKEND=cpu .
-elif [ "${build_os:0:5}" == macos ] && [ "${build_arch}" == aarch64 ]; then
+if [ "${build_os:0:5}" == macos ] && [ "${build_arch}" == aarch64 ]; then
+	# Configuración específica para macOS en arquitectura ARM
 	cmake -DCMAKE_OSX_ARCHITECTURES=arm64 -DCOMPUTE_BACKEND=cpu .
 else
+	# Configuración genérica para otras plataformas
 	cmake -DCOMPUTE_BACKEND=cpu .
 fi
+
 cmake --build . --config Release
 
 output_dir="output/${build_os}/${build_arch}"
--- .github/scripts/build-cuda.sh
+++ .github/scripts/build-cuda.sh
@@ -4,16 +4,24 @@ declare build_os
 declare cuda_version
 
 set -xeuo pipefail
-build_capability="50;52;60;61;70;75;80;86;89;90;100;120"
-remove_for_11_7=";89;90;100;120"
-remove_for_11_8=";100;120"
-remove_for_lt_12_7=";100;120"
-[[ "${cuda_version}" == 11.7.* ]] && build_capability=$(sed 's|'"$remove_for_11_7"'||g' <<< "$build_capability")
-[[ "${cuda_version}" == 11.8.* ]] && build_capability=$(sed 's|'"$remove_for_11_8"'||g' <<< "$build_capability")
-[[ "${cuda_version}" < 12.7 ]] && build_capability=$(sed 's|'"$remove_for_lt_12_7"'||g; s|'"${remove_for_lt_12_7#;}"';||g' <<< "$build_capability")
+
+# Configuración de capacidades de cómputo
+if [ "${build_arch}" == "aarch64" ]; then
+    build_capability="87;90a;110a;100a;120a"
+else
+    build_capability="50;52;60;61;70;75;80;86;89;90;100;120"
+    remove_for_11_7=";89;90;100;120"
+    remove_for_11_8=";100;120"
+    remove_for_lt_12_7=";100;120"
+
+    [[ "${cuda_version}" == 11.7.* ]] && build_capability=$(sed 's|'"$remove_for_11_7"'||g' <<< "$build_capability")
+    [[ "${cuda_version}" == 11.8.* ]] && build_capability=$(sed 's|'"$remove_for_11_8"'||g' <<< "$build_capability")
+    [[ "${cuda_version}" < 12.7 ]] && build_capability=$(sed 's|'"$remove_for_lt_12_7"'||g; s|'"${remove_for_lt_12_7#;}"';||g' <<< "$build_capability")
+fi
+
 [[ "${build_os}" = windows-* ]] && python3 -m pip install ninja
 
-if [ "${build_os:0:6}" == ubuntu ]; then
+if [ "${build_os:0:6}" == "ubuntu" ] && [ "${build_arch}" == "x86_64" ]; then
     image=nvidia/cuda:${cuda_version}-devel-ubuntu22.04
     echo "Using image $image"
     docker run --platform "linux/$build_arch" -i -w /src -v "$PWD:/src" "$image" sh -c \
@@ -26,7 +34,6 @@ else
     cmake -G Ninja -DCOMPUTE_BACKEND=cuda -DCOMPUTE_CAPABILITY="${build_capability}" -DCMAKE_BUILD_TYPE=Release -S .
     cmake --build . --config Release
 fi
-
 
 output_dir="output/${build_os}/${build_arch}"
 mkdir -p "${output_dir}"
